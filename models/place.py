507a88f8e888723d41e900025e2cd41ef3fe5f4e
MariamNsubuga <30923697+MariamNsubuga@users.noreply.github.com>
2023-03-19 13:23:49 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python3
-""" Place class"""
-import models
+"""This is the place class"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+from os import environ
+from uuid import uuid4
 
 
-class Place(BaseModel, Base):
-    """A place"""
-    if models.storage_t == 'db':
-        __tablename__ = 'places'
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column(
+        'place_id',
+        String(60),
+        ForeignKey('places.id'),
+        nullable=False),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        nullable=False))
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class Place(BaseModel, Base):
+        """This is the class for Place
+        Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+        """
+        __tablename__ = "places"
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -23,10 +49,29 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                 backref="place_amenities",
-                                 viewonly=False)
-    else:
+        amenities = relationship("Amenity",
+                                 secondary=place_amenity, viewonly=False)
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
+    class Place(BaseModel):
+        """This is the class for Place
+        Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+        """
         city_id = ""
         user_id = ""
         name = ""
@@ -38,3 +83,13 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+        @property
+        def reviews(self):
+            all_reviews = models.storage.all(Review)
+            liste = []
+            keys = all_review.items()
+            for i, j in keys:
+                if "Review" == i[0:4] and j.place_id == self.id:
+                    liste.append(j)
+            return(liste)
